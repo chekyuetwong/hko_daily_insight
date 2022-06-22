11f88ef2d6cdc15bcf8a2eaf8cebf671ca8733f7
chekyuetwong <chekyuetwong@msn.com>
2022-06-22 15:39:26 +0100

    Update regional_rain.py

--- regional_rain.py
+++ regional_rain.py
@@ -118,13 +118,12 @@ def region_rain():
         i+=1
         progress=i/len(domain)
         p_bar.progress(progress)
-
     driver.close()
 
     for i in district:
         district_max_h[i]=from_web.loc[from_web["Region"]==i].loc[:, "Rainfall"]  
-
     status.success("Download completed!")
+    
     district_max_h=district_max_h.applymap(out_max, na_action='ignore')
     filename = "max hr regional rain "+date1.strftime("%m%d-%H")+" to "+date2.strftime("%m%d-%H")+".csv"
 
